7d4a62afc8dfcd8cc5d32bfd0e56a6dff51364e3
wxt <1738365614@qq.com>
2021-02-03 15:12:56 +0800

    â修改以后

--- gulimall-pms/src/main/java/xintao/gulimall/pms/GulimallPmsApplication.java
+++ gulimall-pms/src/main/java/xintao/gulimall/pms/GulimallPmsApplication.java
@@ -3,11 +3,13 @@ package xintao.gulimall.pms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("xintao.gulimall.pms.dao")
 @EnableSwagger2
+@EnableDiscoveryClient
 public class GulimallPmsApplication {
 
     public static void main(String[] args) {
